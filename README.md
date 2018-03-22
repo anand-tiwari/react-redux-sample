5d417753220cbf4e730cee1bebe79833171b77a0
anandtiwari <anand.tiwari@gdn-commerce.com>
2018-03-22 16:32:46 +0530

    implementation of redux

--- README.md
+++ README.md
@@ -2432,3 +2432,8 @@ In the future, we might start automatically compiling incompatible third-party m
 ## Something Missing?
 
 If you have ideas for more “How To” recipes that should be on this page, [let us know](https://github.com/facebookincubator/create-react-app/issues) or [contribute some!](https://github.com/facebookincubator/create-react-app/edit/master/packages/react-scripts/template/README.md)
+
+## How to create and run app
+npm i -g create-react-app
+create-react-app appname
+npm start
--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postActions';
 
 class Postform extends Component {
 
@@ -25,16 +28,7 @@ class Postform extends Component {
 			title:  this.state.title,
 			body: this.state.body
 		}
-
-		fetch('https://jsonplaceholder.typicode.com/posts', {
-			method: 'POST',
-			headers: {
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify(post)
-		})
-		.then(res => res.json())
-		.then(data => console.log(data));
+	    this.props.createPost(post);
 	}
 
 	render() {
@@ -62,4 +56,9 @@ class Postform extends Component {
 
 }
 
-export default Postform;
+Postform.PropTypes = {
+    createPost: PropTypes.func.isRequired
+}
+
+export default connect(null, { createPost })(Postform);
+//export default Postform;
